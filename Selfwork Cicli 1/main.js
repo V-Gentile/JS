d019db86ac47a0be2c1573b395a688a7dd32d06b
V-Gentile <Vincen.gentile@gmail.com>
2026-05-30 02:09:07 +0200

    Add files via upload

---
+++ Selfwork Cicli 1/main.js
@@ -0,0 +1,10 @@
+let num = 2;
+
+console.log(`TABELLINA DEL ${num}`);
+
+for (let i = 1; i <= 10; i++) {
+    
+    let risultato = num * i;
+    
+    console.log(`${num} x ${i} = ${risultato}`);
+}
